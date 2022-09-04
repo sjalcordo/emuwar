f6ddf295bbb3bed6a501196d9a5ff126889ec5a9
PixelTaku <110940348+PixelTaku@users.noreply.github.com>
2022-09-04 16:35:29 -0400

    added main menu sounds & music

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,10 +7,18 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
 
+    public AudioSource bkgdMusic;
+    private static AudioSource _instance;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (GameObject.Find("bgm").GetComponent<AudioSource>() != null
+            && !GameObject.Find("bgm").GetComponent<AudioSource>().isPlaying)
+        {
+            GameObject.Find("bgm").GetComponent<AudioSource>().Play();
+        }
     }
 
     // Update is called once per frame
@@ -31,13 +39,43 @@ public class MenuManager : MonoBehaviour
         switch (i)
         {
             case 0: //start
-                //add sfx
-                SceneManager.LoadScene(1);
+                StartCoroutine("StartSequence");
                 break;
             case 1: // quit
+                Destroy(GameObject.Find("bgm"));
                 Application.Quit();
                 break;
         }
     }
+
+    IEnumerator StartSequence()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(1);
+    }
+
+    // persisting bgm
+    public static AudioSource Instance
+    {
+        // prevents accessing the raw variable
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        // already initialized and isn't the current song
+        if (_instance != null && _instance != this.bkgdMusic)
+        {
+            // delete any new instances
+            Destroy(bkgdMusic);
+            return;
+        }
+        // set instance to current object
+        _instance = this.bkgdMusic;
+
+        // persisting bkgd music
+        DontDestroyOnLoad(bkgdMusic);
+    }
+
 
 }
